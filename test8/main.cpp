e9c1fa860b978ad49ae4e7162ab1fc74e31a1c6a
han <han@handeMacBook-Pro.local>
2018-11-25 23:53:13 +0800

    Initial Commit

---
+++ test8/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  test8
+//
+//  Created by han on 2018/11/25.
+//  Copyright © 2018 han. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
